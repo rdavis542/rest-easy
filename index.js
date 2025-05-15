d6ae52eb6940c717faa5c08e8d0ef058083893cb
Ryan Davis <ryan_davis542@outlook.com>
2025-05-15 16:34:10 -0400

    remove security

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const fs = require('fs');
 const logger = require('./middleware/logger');
 const bodyParser = require('body-parser');
 
-const options = {
-    key: fs.readFileSync("./keys/privkey8.pem"),
-    cert: fs.readFileSync("./keys/fullchain8.pem")
-  };
-
 app.use(logger);
 app.use(express.json());
 app.use(bodyParser.json());
@@ -22,7 +17,6 @@ app.use('/dataread', require('./routes/dataread'));
 app.use('/dataquery', require('./routes/dataquery'));
 app.use('/users', require('./routes/users'));
 
-//https.createServer(options, app).listen(8443, () => console.log (`Secure listener start on port 8443`));
 
 var server = app.listen(8080, function () {
     var host = server.address().address
